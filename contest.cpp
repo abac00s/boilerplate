2c38ccdd4607e1efe21c98940a0819ce32c182dc
Adam Bac <me@adambac.com>
2017-11-30 18:55:41 +0100

    Added contest boilerplate

---
+++ contest.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define REP(i, n) for(int i=0; i<(n); i++)
+#define FOR(i, s, n) for(int i=(s); i<=(n); i++)
+#define FORD(i, s, n) for(int i=(s); i>=(n); i--)
+#define SIZE(x) ((int) (x).size())
+#define D if(1)
+typedef long long ll;
+typedef vector<int> VI;
+typedef vector<VI> VII;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+
+}
